55a20970b6fda512c2f060c9e56158de72e38a6e
Johannes Westlund <johannes@westlundarna.se>
2017-03-31 21:40:04 +0200

    back-button and layout-tweak

--- app/src/main/java/se/acoder/autohub/hub/products/Phone/PhoneHubFragment.java
+++ app/src/main/java/se/acoder/autohub/hub/products/Phone/PhoneHubFragment.java
@@ -15,10 +15,15 @@ import se.acoder.autohub.hub.products.ProductFragment;
 
 public class PhoneHubFragment extends ProductFragment {
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.product_view_layout, container, false);
+        View rootView = super.onCreateView(inflater,container,savedInstanceState);
         ViewGroup productView= (ViewGroup) rootView.findViewById(R.id.product_view);
         View phoneView = inflater.inflate(R.layout.phone_main, productView, false);
         productView.addView(phoneView);
